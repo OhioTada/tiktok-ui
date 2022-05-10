f1a4864b82c16705962f300c459b417af65d0e1e
Ohiotada <ohiotada@gmail.com>
2022-05-10 22:20:53 +0700

    up

---
+++ src/routes/index.js
@@ -0,0 +1,25 @@
+//Layout
+import {HeaderOnly} from '~/components/Layout';
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+
+const publicRoutes =[
+    // các Router không cần đăng nhập vẫn vào được
+    {path: '/', component: Home},
+    {path: '/following', component: Following},
+    {path: '/profile', component: Profile},
+    {path: '/upload', component: Upload, layout:HeaderOnly},
+    {path: '/search', component: Search, layout:null},
+]
+
+
+const privateRoutes =[
+    // các Router cần đăng nhập vẫn vào được
+
+]
+
+export {publicRoutes, privateRoutes}; 
